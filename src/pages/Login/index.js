7268117ccca2c53d2c5ae4e45a190c79a4f8dadc
Pedro <pedromoratelli@gmail.com>
2020-11-24 19:56:32 -0300

    Add new login screen style

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,33 +1,34 @@
-import React, { useState, useEffect } from 'react';
-import { useHistory, useLocation, Link } from 'react-router-dom';
-import Container from 'react-bootstrap/Container';
-import Col from 'react-bootstrap/Col';
-import Row from 'react-bootstrap/Row';
-import Alert from 'react-bootstrap/Alert';
-import Form from 'react-bootstrap/Form';
-import Button from 'react-bootstrap/Button';
-import Collapse from 'react-bootstrap/Collapse';
+import React, { useState, useEffect } from "react";
+import { useHistory, useLocation, Link } from "react-router-dom";
+import Container from "react-bootstrap/Container";
+import Col from "react-bootstrap/Col";
+import Row from "react-bootstrap/Row";
+import Alert from "react-bootstrap/Alert";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Collapse from "react-bootstrap/Collapse";
 
-import api from '../../services/api';
-import { login, setUserData } from '../../services/auth';
+import api from "../../services/api";
+import { login, setUserData } from "../../services/auth";
 
-import './styles.css';
+import logo from "./poup-logo-no-shadow.png";
+import "./styles.css";
 
 const Login = () => {
   const location = useLocation();
   const history = useHistory();
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
   const [showError, setShowError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState("");
   const [showSuccess, setShowSuccess] = useState(false);
-  const [successMessage, setSuccessMessage] = useState('');
+  const [successMessage, setSuccessMessage] = useState("");
   const [validated, setValidated] = useState(false);
 
   useEffect(() => {
     if (location.state?.missingAuth) {
       setShowError(true);
-      setErrorMessage('É necessário se autenticar para acessar o sistema!');
+      setErrorMessage("É necessário se autenticar para acessar o sistema!");
     }
     if (location.state?.successMessage) {
       setShowSuccess(true);
@@ -44,7 +45,7 @@ const Login = () => {
       setValidated(true);
     } else {
       try {
-        const response = await api.post('/api/auth/login', {
+        const response = await api.post("/api/auth/login", {
           username: email,
           password,
         });
@@ -55,11 +56,11 @@ const Login = () => {
           email: response.data.email,
         });
         setValidated(true);
-        history.push('/dashboard');
+        history.push("/dashboard");
       } catch (err) {
         setValidated(false);
         setShowError(true);
-        setErrorMessage('Email e/ou senha inválido!');
+        setErrorMessage("Email e/ou senha inválido!");
       }
     }
   };
@@ -67,15 +68,11 @@ const Login = () => {
   return (
     <Container fluid id="login-container">
       <Row noGutters className="h-100 align-items-center">
-        <Col md="7">
-          <picture id="login-picture">
-            <img src="" alt="stonks" />
-          </picture>
-        </Col>
+        <Col md="7"></Col>
         <Col md="5">
           <section id="login-box" className="text-center col-md-6 mx-auto">
             <header className="mb-5">
-              <img src="../img/logo-dark.png" alt="Logo" />
+              <img src={logo} alt="Logo" />
             </header>
             <Form
               id="login-form"
