56e0a5cdc4f92889d06904b6f0f2b5e7bae2b111
Pedro <pedromoratelli@gmail.com>
2020-11-23 21:19:14 -0300

    Refactor login page integration tests

--- cypress/integration/login-page/login.js
+++ cypress/integration/login-page/login.js
@@ -7,34 +7,34 @@ describe("Login", () => {
   const validPassword = "123";
 
   it("Successful login", () => {
-    cy.get("[class=form-control]").eq(0).type(validLogin);
-    cy.get("[class=form-control]").eq(1).type(validPassword);
+    cy.get("[id=user-email]").type(validLogin);
+    cy.get("[id=user-password]").type(validPassword);
     cy.get("[id='login-button']").click();
     cy.document().should("contain.text", "Logout");
   });
 
   it("User inserted the right login and wrong password", () => {
-    cy.get("[class=form-control]").eq(0).type(validLogin);
-    cy.get("[class=form-control]").eq(1).type("000000");
+    cy.get("[id=user-email]").type(validLogin);
+    cy.get("[id=user-password]").type("000000");
     cy.get("[id='login-button']").click();
     cy.document().should("contain.text", "Email e/ou senha inválido!");
   });
 
   it("User inserted the wrong login and right password", () => {
-    cy.get("[class=form-control]").eq(0).type("testessssss@cypress.com");
-    cy.get("[class=form-control]").eq(1).type(validPassword);
+    cy.get("[id=user-email]").type("testessssss@cypress.com");
+    cy.get("[id=user-password]").type(validPassword);
     cy.get("[id='login-button']").click();
     cy.document().should("contain.text", "Email e/ou senha inválido!");
   });
 
   it("User inserted a login but didn't insert a password", () => {
-    cy.get("[class=form-control]").eq(0).type(validLogin);
+    cy.get("[id=user-email]").type(validLogin);
     cy.get("[id='login-button']").click();
     cy.document().should("contain.text", "Informe um e-mail válido.");
   });
 
   it("User didn't insert a login but inserted a password", () => {
-    cy.get("[class=form-control]").eq(0).type(validPassword);
+    cy.get("[id=user-password]").type(validPassword);
     cy.get("[id='login-button']").click();
     cy.document().should("contain.text", "Infome sua senha.");
   });
