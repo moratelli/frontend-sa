e9286aecb32168ed1bc39fc98a63cc73c1ee5bee
Thiago Panchiniak <panchiniak@gmail.com>
2020-11-25 20:28:13 -0300

    create select charts in report

--- src/pages/Report/index.js
+++ src/pages/Report/index.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
+import { Form } from 'react-bootstrap';
 import Container from 'react-bootstrap/Container';
-import { Bar, Pie, HorizontalBar } from 'react-chartjs-2';
+import { Bar, Pie, HorizontalBar, Line } from 'react-chartjs-2';
 import api from '../../services/api';
 import { getUserData } from '../../services/auth';
 
@@ -36,6 +37,7 @@ const data = {
 const Report = () => {
   const [transactions, setTransactions] = useState([]);
   const [flowChartData, setFlowChartData] = useState([]);
+  const [inCategoryChartData, setInCategoryChartData] = useState([])
   const { userId } = getUserData();
 
   useEffect(() => {
@@ -62,10 +64,63 @@ const Report = () => {
     setFlowChartData([totalIn, totalOut]);
   }, [transactions]);
 
+  useEffect(() => {
+    const salarioTransactions = transactions.filter((t) => t.category === 'SALARIO');
+    const totalSalario = salarioTransactions.reduce((total, cur) => total + cur.value, 0);
+
+    const apostaTransactions = transactions.filter((t) => t.category === 'APOSTA');
+    const totalAposta = apostaTransactions.reduce((total, cur) => total + cur.value, 0);
+
+    const freelanceTransactions = transactions.filter((t) => t.category === 'FREELANCE');
+    const totalFreelance = freelanceTransactions.reduce((total, cur) => total + cur.value, 0);
+
+    setInCategoryChartData([totalSalario, totalAposta, totalFreelance]);
+  }, [transactions]);
+
   return (
     <Container fluid id="report-container">
+
       <h1>Relatórios</h1>
       <br />
+      <select>
+          <option value="InOutTotal">Total de Entradas e Saídas</option>
+          <option value="InCategories">Total de Entradas por Categoria</option>
+          <option value="OutCategories">Total de Saídas por Categoria</option>
+          <option value="InDateTime">Total de Entradas por Data</option>
+          <option value="OutDateTime">Total de Saídas por Data</option>
+          <option value="InOutCategories">Total de Entradas e Saídas por Categoria</option>
+          <option value="InOutDateTime">Total de Entradas e Saídas por Data</option>
+      </select>
+      <br />
+      <Form.Group>
+        <Form.Row>
+          <Form.Check
+            type="radio"
+            id="chart"
+            label="Pizza"
+            name="flow"
+          />
+
+          <Form.Check
+            type="radio"
+            id="chart"
+            label="Barra Horizontal"
+            name="flow"
+          />
+          <Form.Check
+            type="radio"
+            id="chart"
+            label="Barra Vertical"
+            name="flow"
+          />
+          <Form.Check
+            type="radio"
+            id="chart"
+            label="Linha Horizontal"
+            name="flow"
+          />
+        </Form.Row>
+      </Form.Group>
       <div>
         <Pie
           options={{
@@ -97,6 +152,21 @@ const Report = () => {
               },
             ],
           })}
+        />
+        <Bar
+          width={100}
+          height={50}
+          data={data}
+        />
+        <HorizontalBar
+          width={100}
+          height={50}
+          data={data}
+        />
+        <Line
+          width={100}
+          height={50}
+          data={data}
         />
       </div>
     </Container>
