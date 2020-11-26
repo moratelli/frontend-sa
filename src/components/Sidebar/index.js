513950248d2578e71089ef404b00e0ef7911d9b1
Pedro <pedromoratelli@gmail.com>
2020-11-25 22:16:05 -0300

    Change sidebar style

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,50 +1,65 @@
-import React from 'react';
-import { Nav, Button } from 'react-bootstrap';
-import { Link, useHistory, useLocation } from 'react-router-dom';
+import React from "react";
+import { Nav, Button } from "react-bootstrap";
+import { Link, useHistory, useLocation } from "react-router-dom";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faUser,
+  faHome,
+  faChartLine,
+  faPlus,
+} from "@fortawesome/free-solid-svg-icons";
 
-import './styles.css';
-import { logout } from '../../services/auth';
+import logo from "../../img/poup-logo-no-shadow.png";
+
+import "./styles.css";
+import { logout } from "../../services/auth";
 
 const Sidebar = () => {
   const history = useHistory();
   const location = useLocation();
-  const [path] = location.search.split('=');
+  const [path] = location.search.split("=");
 
   const handleLogout = () => {
     logout();
-    history.push('/');
+    history.push("/");
   };
 
   return (
     <>
-      <Nav
-        className="col-md-12 pr-0 d-none d-md-block bg-light sidebar"
-        activeKey={path}
-        variant="pills"
-      >
-        <Nav.Item>
-          <Nav.Link eventKey="?profile" as={Link} to="/dashboard?profile">
-            Perfil
-          </Nav.Link>
-        </Nav.Item>
+      <Nav className="col-md-12 sidebar" activeKey={path} variant="pills">
+        <header className="mb-5">
+          <img src={logo} alt="Logo" />
+        </header>
+
         <Nav.Item>
           <Nav.Link eventKey="?show" as={Link} to="/dashboard?show">
+            <FontAwesomeIcon icon={faHome} />
             Home
           </Nav.Link>
         </Nav.Item>
         <Nav.Item>
           <Nav.Link eventKey="?report" as={Link} to="/dashboard?report">
+            <FontAwesomeIcon icon={faChartLine} />
             Relatórios
           </Nav.Link>
         </Nav.Item>
         <Nav.Item>
           <Nav.Link eventKey="?create" as={Link} to="/dashboard?create">
+            <FontAwesomeIcon icon={faPlus} />
             Criar
           </Nav.Link>
         </Nav.Item>
         <Nav.Item>
+          <Nav.Link eventKey="?profile" as={Link} to="/dashboard?profile">
+            <FontAwesomeIcon icon={faUser} />
+            Perfil
+          </Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
           <Nav.Link>
-            <Button onClick={handleLogout}>Logout</Button>
+            <Button variant="outline-danger" onClick={handleLogout}>
+              Logout
+            </Button>
           </Nav.Link>
         </Nav.Item>
       </Nav>
