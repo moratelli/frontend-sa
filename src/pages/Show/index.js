8a06059cb15eb7c25fc5b0711b44d2bd0769b565
Pedro <pedromoratelli@gmail.com>
2020-11-26 01:13:39 -0300

    Add calculate spending feature at Home

--- src/pages/Show/index.js
+++ src/pages/Show/index.js
@@ -7,10 +7,11 @@ import Row from "react-bootstrap/Row";
 import "./styles.css";
 import api from "../../services/api";
 import { getUserData } from "../../services/auth";
+import thanosMeme from "../../img/thanos.jpg";
 
 const Show = () => {
   const [transactions, setTransactions] = useState([]);
-  const { userId } = getUserData();
+  const { userId, name } = getUserData();
 
   useEffect(() => {
     const fetchData = async () => {
@@ -27,7 +28,7 @@ const Show = () => {
   }, [userId]);
 
   const createTableRows = () =>
-    transactions.map((transaction) => (
+    transactions.reverse().map((transaction) => (
       <tr key={transaction.id}>
         <td>R$ {transaction.value}</td>
         <td>{transaction.description}</td>
@@ -36,10 +37,81 @@ const Show = () => {
       </tr>
     ));
 
+  const calculateSpending = () => {
+    const lastTenTransactions = transactions.reverse().slice(0, 10);
+
+    console.log(lastTenTransactions);
+
+    let inSum = 0;
+    let outSum = 0;
+
+    lastTenTransactions.forEach((transaction) => {
+      if (transaction.flow === "IN") inSum += transaction.value;
+      if (transaction.flow === "OUT") outSum += transaction.value;
+    });
+
+    return (
+      <>
+        <br></br>
+        <h4>
+          <span role="img" aria-label="money-flying">
+            💸
+          </span>{" "}
+          Gastou R$ {outSum}
+        </h4>
+        <h6>A categoria em que mais gastou foi abc!</h6>
+        <br></br>
+        <h4>
+          <span role="img" aria-label="money-flying">
+            💰
+          </span>{" "}
+          Recebeu R$ {inSum}
+        </h4>
+        <h6>A categoria em que mais recebeu foi abc!</h6>
+        <br></br>
+        {inSum > outSum ? (
+          <>
+            <h3>
+              Parabéns!{" "}
+              <span role="img" aria-label="confetti">
+                🎉
+              </span>{" "}
+            </h3>
+            <h5>Você tem recebido mais do que gasto. Continue assim!</h5>
+          </>
+        ) : outSum > inSum ? (
+          <>
+            <h3>
+              Cuidado!{" "}
+              <span role="img" aria-label="warning-sign">
+                ⚠️
+              </span>{" "}
+            </h3>
+            <h5>
+              Você tem gasto mais do que recebido. Tente controlar mais os seus
+              gastos!
+            </h5>
+          </>
+        ) : (
+          <img
+            src={thanosMeme}
+            alt="Perfectly balanced!"
+            style={{ maxWidth: 500 }}
+          />
+        )}
+      </>
+    );
+  };
+
   return (
     <Container fluid id="show-container">
-      <Row fluid noGutters className="h-100 w-100 align-items-center">
-        <Col xs={6}></Col>
+      <Row noGutters className="h-100 w-100 align-items-center">
+        <Col xs={6}>
+          <h1>Olá, {name}! 👋</h1>
+          <br></br>
+          <h3>Nas suas últimas dez transações, percebi que você...</h3>
+          {calculateSpending()}
+        </Col>
         <Col xs={6}>
           <h2>Últimos registros</h2>
           <br></br>
