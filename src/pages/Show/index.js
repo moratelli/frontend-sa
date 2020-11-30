3fe99cc32bd0daf3d3a693fee2d44777ca01492b
ArthurUFelix <arthurufelix@gmail.com>
2020-11-29 22:48:51 -0300

    Fix build errors

--- src/pages/Show/index.js
+++ src/pages/Show/index.js
@@ -37,7 +37,7 @@ const Show = () => {
     };
 
     fetchData();
-  }, [userId, currentPage]);
+  }, [userId, currentPage, itemsPerPage, lastTenTransactions]);
 
   const createTableRows = () => transactions.map((transaction) => (
     <tr
